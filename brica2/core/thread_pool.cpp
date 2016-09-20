b34a83e46a98e29868a880f0f5b203b23139d191
ktnyt <kotone@sfc.keio.ac.jp>
2016-09-20 10:15:51 +0900

    Fix file preamble

--- brica2/core/thread_pool.cpp
+++ brica2/core/thread_pool.cpp
@@ -2,6 +2,11 @@
  *
  * brica2/core/thread_pool.cpp
  *
+ * @author Copyright (C) 2016 Kotone Itaya
+ * @version 1.0.0
+ * @created  2016/09/18 Kotone Itaya -- Created!
+ * @@
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
