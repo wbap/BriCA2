c9d60da5bd6fa6bf7aab0f1d1c2d3a9fcc8643b0
ktnyt <kotone@sfc.keio.ac.jp>
2016-10-08 03:14:20 +0900

    Rename `range_x()` and `range_y()` to `range()`

--- tests/brica2/core/vector_bm.cpp
+++ tests/brica2/core/vector_bm.cpp
@@ -7,7 +7,7 @@
 namespace bc = brica2::core;
 
 static void Vector1dSimple(benchmark::State& state) {
-  std::size_t size = static_cast<std::size_t>(state.range_x());
+  std::size_t size = static_cast<std::size_t>(state.range());
 
   bc::Vector<int> v({size});
   bc::Vector<int> u({size});
@@ -18,7 +18,7 @@ static void Vector1dSimple(benchmark::State& state) {
 }
 
 static void Vector1dClone(benchmark::State& state) {
-  std::size_t size = static_cast<std::size_t>(state.range_x());
+  std::size_t size = static_cast<std::size_t>(state.range());
 
   bc::Vector<int> v({size});
   bc::Vector<int> u({size});
@@ -29,8 +29,8 @@ static void Vector1dClone(benchmark::State& state) {
 }
 
 static void Vector2dSimple(benchmark::State& state) {
-  std::size_t size_x = static_cast<std::size_t>(state.range_x());
-  std::size_t size_y = static_cast<std::size_t>(state.range_y());
+  std::size_t size_x = static_cast<std::size_t>(state.range(0));
+  std::size_t size_y = static_cast<std::size_t>(state.range(1));
 
   bc::Vector<int> v({size_x, size_y});
   bc::Vector<int> u({size_x, size_y});
@@ -41,8 +41,8 @@ static void Vector2dSimple(benchmark::State& state) {
 }
 
 static void Vector2dClone(benchmark::State& state) {
-  std::size_t size_x = static_cast<std::size_t>(state.range_x());
-  std::size_t size_y = static_cast<std::size_t>(state.range_y());
+  std::size_t size_x = static_cast<std::size_t>(state.range(0));
+  std::size_t size_y = static_cast<std::size_t>(state.range(1));
 
   bc::Vector<int> v({size_x, size_y});
   bc::Vector<int> u({size_x, size_y});
